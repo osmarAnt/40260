4637bae36e92048f8ffb3202ad890475e0e81fc0
osmarAnt <dontstarvero678@gmai.com>
2026-05-07 12:03:10 -0600

    calculadora inicial

---
+++ javascript/javascript-02.js
@@ -0,0 +1,56 @@
+let resultado=document.getElementById("resultado")
+let botones =document.getElementsByTagName("button")
+let r=0
+let eliminar = 0
+let operacion = ""
+for (const key in botones) {
+    if (Object.prototype.hasOwnProperty.call(botones,key)){
+    const boton =botones[key]
+    if (boton.className != "operadores"){
+        boton.addEventListener("click",pintar)}
+    else {
+        boton.addEventListener("click",pintar2)
+    }
+
+    
+    }
+}
+function pintar(e){
+    console.log(e.target.innerText)
+    //         if (eliminar === 1){
+    //         resultado.value=""
+    //         eliminar = 0
+    //     }
+    resultado.value += e.target.innerText
+    
+}
+
+function pintar2(e){
+ console.log(e.target.innerHTML)
+}
+
+function suma(){
+    // r= resultado.value
+        r = eval(r + parseInt(resultado.value))
+    resultado.value=""
+    operacion = "+"
+//  console.log(r)
+}
+function respuesta(){
+    if (operacion === "+"){
+        r = eval(r + parseInt(resultado.value))
+        resultado.value = r.toString()
+            console.log(r)
+        r=0
+    }
+    eliminar = 1
+
+}
+
+function resta(){}
+function multiplicacion(){}
+function division(){}
+
+function borrar(){
+    resultado.value=""
+}
