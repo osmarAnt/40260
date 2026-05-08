003092c783c12dcc769062c22154a4b8f83e3d2d
osmarAnt <dontstarvero678@gmai.com>
2026-05-08 11:18:10 -0600

    tarea calculadora

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -17,10 +17,10 @@ for (const key in botones) {
 }
 function pintar(e){
     console.log(e.target.innerText)
-    //         if (eliminar === 1){
-    //         resultado.value=""
-    //         eliminar = 0
-    //     }
+            if (eliminar === 1){
+            resultado.value=""
+            eliminar = 0
+        }
     resultado.value += e.target.innerText
     
 }
@@ -29,13 +29,7 @@ function pintar2(e){
  console.log(e.target.innerHTML)
 }
 
-function suma(){
-    // r= resultado.value
-        r = eval(r + parseInt(resultado.value))
-    resultado.value=""
-    operacion = "+"
-//  console.log(r)
-}
+
 function respuesta(){
     if (operacion === "+"){
         r = eval(r + parseInt(resultado.value))
@@ -43,13 +37,50 @@ function respuesta(){
             console.log(r)
         r=0
     }
+    if (operacion === "-"){
+        r = eval(r - parseInt(resultado.value))
+        resultado.value = r.toString()
+            console.log(r)
+        r=0
+    }
+    if (operacion === "*"){
+        r = eval(r * parseInt(resultado.value))
+        resultado.value = r.toString()
+            console.log(r)
+        r=0
+    }
+    if (operacion === "/" && resultado.value!=0){
+        r = eval(r / parseInt(resultado.value))
+        resultado.value = r.toString()
+            console.log(r)
+        r=0
+
+    }
     eliminar = 1
 
 }
-
-function resta(){}
-function multiplicacion(){}
-function division(){}
+function suma(){
+    // r= resultado.value
+        r = eval(r + parseInt(resultado.value))
+    resultado.value=""
+    operacion = "+"
+//  console.log(r)
+}
+function resta(){
+    r = eval(r + parseInt(resultado.value))
+    resultado.value=""
+    operacion = "-"
+}
+function multiplicacion(){
+    r = eval(r + parseInt(resultado.value))
+    resultado.value=""
+    operacion = "*"
+}
+function division(){
+    r = eval(r + parseInt(resultado.value))
+    resultado.value=""
+    operacion = "/"
+}
 
 function borrar(){
     resultado.value=""
